73396929e38bf5a543dfe12c8030501bc71786fe
ldemouge <ldemouge@gmail.com>
2018-01-19 23:22:46 +0100

    commit -a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCEhtUFTh_IttqXiJwuY6k21lTIaB6sAtE',
+    authDomain: 'ldg-2-0-1.firebaseapp.com',
+    databaseURL: 'https://ldg-2-0-1.firebaseio.com',
+    //databaseURL: `ws://127.0.1:5000`,
+    projectId: 'ldg-2-0-1',
+    storageBucket: 'ldg-2-0-1.appspot.com',
+    messagingSenderId: '310398173528'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCEhtUFTh_IttqXiJwuY6k21lTIaB6sAtE',
+    authDomain: 'ldg-2-0-1.firebaseapp.com',
+    databaseURL: 'https://ldg-2-0-1.firebaseio.com',
+    //databaseURL: `ws://127.0.1:5000`,
+    projectId: 'ldg-2-0-1',
+    storageBucket: 'ldg-2-0-1.appspot.com',
+    messagingSenderId: '310398173528'
+  }
 };

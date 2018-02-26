11e10c82e8d006d8d6030c8e006b5dcb3c232183
ldemouge <ldemouge@gmail.com>
2018-02-26 11:58:44 +0100

    grid layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = "Logiciel de gestion"
 
 }
